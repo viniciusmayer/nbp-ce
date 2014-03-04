9e2a665a86fd9187ad112717da27a2d322b7af28
Vinicius Mayer <viniciusmayer@gmail.com>
2014-03-04 19:09:54 -0300

    initial commit

---
+++ CRUDExemplo/src/br/com/ftec/exemplo/crud/Mestre.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ftec.exemplo.crud;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author eleonorvinicius
+ */
+@Entity
+@Table(name = "mestre", catalog = "test", schema = "public")
+@NamedQueries({
+    @NamedQuery(name = "Mestre.findAll", query = "SELECT m FROM Mestre m"),
+    @NamedQuery(name = "Mestre.findById", query = "SELECT m FROM Mestre m WHERE m.id = :id"),
+    @NamedQuery(name = "Mestre.findByNome", query = "SELECT m FROM Mestre m WHERE m.nome = :nome"),
+    @NamedQuery(name = "Mestre.findByDescricao", query = "SELECT m FROM Mestre m WHERE m.descricao = :descricao")})
+public class Mestre implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "descricao")
+    private String descricao;
+
+    public Mestre() {
+    }
+
+    public Mestre(Integer id) {
+        this.id = id;
+    }
+
+    public Mestre(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Integer oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        changeSupport.firePropertyChange("nome", oldNome, nome);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        String oldDescricao = this.descricao;
+        this.descricao = descricao;
+        changeSupport.firePropertyChange("descricao", oldDescricao, descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Mestre)) {
+            return false;
+        }
+        Mestre other = (Mestre) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.ftec.exemplo.crud.Mestre[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
---
+++ CRUDExemploNetbeans/CRUDExemplo/build/classes-generated/br/com/ftec/exemplo/crud/Bundle.java
@@ -0,0 +1,27 @@
+package br.com.ftec.exemplo.crud;
+/** Localizable strings for {@link br.com.ftec.exemplo.crud}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>CRUDExemplo</i>
+     * @see CRUDExemploTopComponent
+     */
+    static String CTL_CRUDExemploAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CRUDExemploAction");
+    }
+    /**
+     * @return <i>CRUDExemplo Window</i>
+     * @see CRUDExemploTopComponent
+     */
+    static String CTL_CRUDExemploTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CRUDExemploTopComponent");
+    }
+    /**
+     * @return <i>This is a CRUDExemplo window</i>
+     * @see CRUDExemploTopComponent
+     */
+    static String HINT_CRUDExemploTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CRUDExemploTopComponent");
+    }
+    private void Bundle() {}
+}
